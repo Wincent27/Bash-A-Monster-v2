cfdc8fcb7957ee4e9fce0259182421ab87544b3c
wincent.nordlanderw <wincent.nordlanderwinback@elev.ga.ntig.se>
2024-01-23 10:15:01 +0100

    Dag 3, senare på dagen kl 10 fortsatte jag med arbetet på Hero klassen. Jag tror att jag har avslutat arbetet på den klassen, men kan inte veta säkert än då jag inte har testkört programmet.

--- src/Hero.java
+++ src/Hero.java
@@ -28,4 +28,53 @@ public class Hero {
         }
             return extraDamage;
     }
+
+    public void chooseAbility(Ability ability) {
+        abilities.add(ability);
+    }
+
+    public void changeWeapon(Weapon newWeapon) {
+        this.weapon = newWeapon;
+        System.out.println(name + "har bytt till" + newWeapon.getClass().getSimpleName());
+    }
+    public int getHealth(){
+        return health;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        if(health < 0){
+            health = 0;
+        }
+    }
+
+    public boolean isDefeated(){
+        return health <= 0;
+    }
+
+    public static Hero createHero(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Ange hjältens namn: ");
+        String heroName = scan.nextLine();
+
+        System.out.println("Välj vapen till din hjälte: ");
+        System.out.println("1. Svärd");
+        System.out.println("2. Pilbåge");
+
+        int weaponChoice = scan.nextInt();
+        Weapon selectedWeapon;
+
+        if(weaponChoice == 1){
+            selectedWeapon = new Sword();
+        } else if (weaponChoice == 2){
+            selectedWeapon = new Bow();
+        } else{
+            System.out.println("Ogiltigt val. Standardvapnet Svärd används!");
+            selectedWeapon = new Sword();
+        }
+
+        return new Hero(heroName, 100, selectedWeapon, new ArrayList<>());
+
+    }
 }
