2d99e24ba7767ca1e904a298bb2b768a820663a0
wincent.nordlanderw <wincent.nordlanderwinback@elev.ga.ntig.se>
2024-01-23 09:07:58 +0100

    Dag 3 på detta projekt. Jag jobbar fortfarande hemmifrån på grund av att jag är sjuk. Började skriva under klassen Hero, fortsätter troligen senare idag.

--- src/Hero.java
+++ src/Hero.java
@@ -1,5 +1,31 @@
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hero {
+    private String name;
+    private int health;
+    private Weapon weapon;
+    private List<Ability> abilities;
+
+    public Hero(String name, int health, Weapon weapon, List<Ability> abilities){
+        this.name = name;
+        this.health = health;
+        this.weapon = weapon;
+        this.abilities = abilities;
+    }
+
+    public Range attack() {
+        Range baseDamage = weapon.attack();
+        int totalDamage = baseDamage.getMax() + calculateExtraDamage();
+        return new Range(totalDamage, totalDamage);
+    }
+
+    private int calculateExtraDamage() {
+        int extraDamage = 0;
+        for(Ability ability : abilities) {
+            extraDamage += ability.getExtraDamage();
+        }
+            return extraDamage;
+    }
 }
