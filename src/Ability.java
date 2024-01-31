12df0e68c8de28d66d11b792b2188a87d14dc3bf
wincent.nordlanderw <wincent.nordlanderwinback@elev.ga.ntig.se>
2024-01-31 12:47:33 +0100

    Kommenterade Ability klassen och Boss klassen. Fortätter troligen idag lite senare med att kommentera de olika klasserna

--- src/Ability.java
+++ src/Ability.java
@@ -1,12 +1,15 @@
-public class Ability {
+public class Ability { // Definierar Java-klassen med namnet "Ability".
     private String name;
     private int extraDamage;
+    // ^ Privata instansvariabler för namn och extra skada.
 
+    // Konstruktor för instansiering av Ability med namn och extra skada.
     public Ability(String name, int extraDamage){
         this.name = name;
         this.extraDamage = extraDamage;
     }
 
+    // Getter-metod för att hämta extra skada.
     public int getExtraDamage() {
         return extraDamage;
     }
--- src/Boss.java
+++ src/Boss.java
@@ -1,19 +1,22 @@
 import java.util.List;
 
-public class Boss extends Monster {
-    private List<Ability> bossAbilities;
+public class Boss extends Monster { //Definierar klassen Boss som en subklass till Monster.
+    private List<Ability> bossAbilities; // Privat variabel för bossens förmågor (en lista av Ability-objekt).
 
+    // Konstruktor som tar emot namn, attackskada, hälsa och bossens förmågor.
+    // Anropar superklassens konstruktor och tilldelar bossAbilities.
     public Boss(String name, Range attackDamage, int health, List<Ability> bossAbilities){
         super(name, attackDamage, health);
         this.bossAbilities = bossAbilities;
     }
 
+    // Getter för bossens förmågor.
     public List<Ability> getBossAbilities(){
         return bossAbilities;
     }
-    public void spceialAttack() {
 
-        //Skapa specialattacks logiken
+    // Metod för en speciell attack. Skriver ut ett meddelande till konsolen.
+    public void spceialAttack() {
         System.out.println("Bossen gör en spedcial attack!");
     }
 }
